b855896b35613a3cc80ad1d9cf9681c1e52c4e68
Raghavan Chandrabalan <raghavan.chandrabalan@purajuniper.com>
2022-05-02 17:26:18 -0400

    add basic support for dynamicValues where the path contains an array

--- src/utils.js
+++ src/utils.js
@@ -78,6 +78,64 @@ export function shouldTryToStringify(elementPath, elementValue) {
   }
 }
 
+// Use at own risk
+// TODO: remake this using js proxies (likely much shorter too)
+export function writeValueToPath(original_obj, path, value) {
+  console.log(path, value);
+  const path_parts = path.split('.');
+  const new_acc = {
+    ...original_obj
+  };
+  let write_to = new_acc;
+  for (let i = 0; i < path_parts.length; i++) {
+    const path_part = path_parts[i];
+    const arr_symbol_idx = path_part.indexOf('[');
+    if (arr_symbol_idx !== -1) {
+      let [part_stripped, arr_idx] = path_part.split('[');
+      arr_idx = arr_idx.slice(0, -1);
+      if (i === path_parts.length - 1) {
+        if (!(part_stripped in write_to) || !Array.isArray(write_to[part_stripped])) {
+          write_to[part_stripped] = [value];
+        }
+        else {
+          if (write_to[part_stripped].length > arr_idx) {
+            write_to[part_stripped] = value;
+          }
+          else {
+            // Wrong, but works for now
+            write_to[part_stripped].push(value);
+          }
+        }
+      }
+      else {
+        if (!(part_stripped in write_to) || !Array.isArray(write_to[part_stripped])) {
+          write_to[part_stripped] = [];
+        }
+        write_to = write_to[part_stripped]
+        if (write_to.length > arr_idx) {
+          write_to = write_to[arr_idx];
+        }
+        else {
+          // Wrong, but works for now
+          write_to = write_to[write_to.push({}) - 1];
+        }
+      }
+    }
+    else {
+      if (i === path_parts.length - 1) {
+        write_to[path_part] = value;
+      }
+      else {
+        if (!(path_part in write_to)) {
+          write_to[path_part] = {};
+        }
+        write_to = write_to[path_part]
+      }
+    }
+  }
+  return new_acc;
+}
+
 /**
  * Takes a FHIRPath to a choice element and converts the element name to include 
  * the appropriate type. This function is needed because in FHIRPath choice elements 
@@ -168,6 +226,26 @@ export function expandPathAndValue(path, value) {
   // Parse the JSON string.
   let expandedObject = JSON.parse(jsonString);
   return expandedObject;
+}
+
+/**
+ * 
+ * @param {object (FHIR Library)} library - Library to search
+ * @param {boolean} inNode - True if base64 conversion should be done using Node functions
+ * instead of browser functions
+ * @returns {object | undefined} - Parsed ELM JSON, if it exists
+ */
+export function getElmJsonFromLibrary(library, inNode=true) {
+  for (const libraryContent of library.content) {
+    if (libraryContent.contentType == "application/elm+json") {
+      if (inNode) {
+        return JSON.parse(Buffer.from(libraryContent.data,'base64').toString('ascii'));
+      }
+      else {
+        return JSON.parse(window.atob(libraryContent.data)); // TODO: Throw error on no data
+      }
+    }
+  }
 }
 
 // From: https://www.hl7.org/fhir/choice-elements.json
